90deea80ece912d901cef86ce8371b798619a78f
Unknown <nas244@msstate.edu>
2019-03-04 20:41:19 -0600

    Test Unity Import

---
+++ Solidworks VR Senior Project/Solidworks VR Senior Project/Assets/Scripts/ImportScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class ImportScript : MonoBehaviour
+{
+    string path;
+    public RawImage image;
+
+    public void OpenImportModels()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with stl", "", "stl");
+    }
+}
